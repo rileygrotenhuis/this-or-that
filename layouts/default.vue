d42a090601fb26213b36f7159ea909b0a039dc5e
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-12-03 16:26:54 -0600

    feat: create basic navbar.

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <nav>
+            <img src="~/assets/Logo.png" width="50px" height="50px" />
+            <h1>This or That</h1>
+        </nav>
+        <Nuxt />
+    </div>
+</template>
