c6b624d2b3af2246aafdfca750666a3d13836c20
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-12-03 15:22:06 -0600

    feat: endpoint for retrieving questions.

--- api/index.js
+++ api/index.js
@@ -8,8 +8,22 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
+const { PrismaClient } = require('@prisma/client');
+
 app.get('/api', (req, res) => {
     return res.json('This or That - API');
+});
+
+app.get('/api/questions', async (req, res) => {
+    const prisma = new PrismaClient();
+
+    const questions = await prisma.questions.findMany({
+        include: {
+            answers: true,
+        },
+    });
+
+    return res.json(questions);
 });
 
 module.exports = app;
