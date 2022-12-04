b1bcdeb2a81981d45c06dbccebb16bbe9d003bd7
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-12-03 21:09:50 -0600

    feat: display total votes for each question on the screen.

--- pages/results.vue
+++ pages/results.vue
@@ -1,7 +1,9 @@
 <template>
     <div v-if="(this.results.length > 0)">
         <h3>{{ this.results[this.currentResult].content }}</h3>
-        <div></div>
+        <div v-for="(value, index) in this.results[this.currentResult].answers" :key="index">
+            <h3> {{value.content}} : {{ results[currentResult].new_submissions[value.id] }} Total Votes</h3>
+        </div>
         <div>
             <button class="result-button" @click="previousQuestion">
                 Last Question
