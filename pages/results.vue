6d083f6103090766d7036c05a25fd52767956f34
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-12-03 21:21:11 -0600

    feat: improve results page.

--- pages/results.vue
+++ pages/results.vue
@@ -1,10 +1,12 @@
 <template>
-    <div v-if="(this.results.length > 0)">
-        <h3>{{ this.results[this.currentResult].content }}</h3>
-        <div v-for="(value, index) in this.results[this.currentResult].answers" :key="index">
-            <h3> {{value.content}} : {{ results[currentResult].new_submissions[value.id] }} Total Votes ({{ getPercentage(value) }}%)</h3>
+    <div v-if="(this.results.length > 0)" class="results-container">
+        <h3 class="results-title">{{ this.results[this.currentResult].content }}</h3>
+        <div class="results-submissions-container">
+            <div v-for="(value, index) in this.results[this.currentResult].answers" :key="index">
+                <h3 class="results-submission-item"> {{value.content}} : {{ results[currentResult].new_submissions[value.id] }} Total Votes ({{ getPercentage(value) }}%)</h3>
+            </div>
         </div>
-        <div>
+        <div class="results-button-container">
             <button class="result-button" @click="previousQuestion">
                 Last Question
             </button>
