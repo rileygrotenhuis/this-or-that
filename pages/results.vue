0cfb09de78693bbb1ae19e3484ae170f22b24564
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-12-03 20:28:57 -0600

    feat: setup basic next and previous question buttons.

--- pages/results.vue
+++ pages/results.vue
@@ -1,5 +1,19 @@
 <template>
-    <h1>Results</h1>
+    <div v-if="(this.results.length > 0)">
+        <h3>{{ this.results[this.currentResult].content }}</h3>
+        <div></div>
+        <div>
+            <button class="result-button" @click="previousQuestion">
+                Last Question
+            </button>
+            <button class="result-button" @click="nextQuestion">
+                Next Question
+            </button>
+        </div>
+    </div>
+    <div v-else>
+        <h3>Retrieving Results...</h3>
+    </div>
 </template>
 
 <script>
@@ -7,11 +21,24 @@ export default {
     name: 'ResultsPage',
     data() {
         return {
+            currentResult: 0,
             results: [],
         };
     },
     async fetch() {
         this.results = await fetch('http://localhost:3000/api/results').then(res => res.json());
+    },
+    methods: {
+        previousQuestion() {
+            if (this.currentResult > 0) {
+                this.currentResult--;
+            }
+        },
+        nextQuestion() {
+            if (this.currentResult < this.results.length -1) {
+                this.currentResult++;
+            }
+        }
     }
 }
 </script>
