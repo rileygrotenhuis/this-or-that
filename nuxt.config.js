a4f06d064f95c54ccf4f9901e462bda8d9d3f26c
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-12-04 16:04:36 -0600

    fix: improve axios config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,8 @@ export default {
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        baseURL: `/api`,
+        baseURL: process.env.BASE_URL,
+        browserBaseURL: process.env.BROWSER_BASE_URL,
     },
 
     serverMiddleware: ['~/api'],
