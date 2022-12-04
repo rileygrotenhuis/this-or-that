8c5e06ba5c918c96faa87b60949346c893bc8935
rileygrotenhuis <rg1050@live.missouristate.edu>
2022-12-04 13:10:07 -0600

    fix: remove trailing slash.

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
     ? 'http://localhost:3000'
-    : 'https://this-or-that-rileygrotenhuis.vercel.app/';
+    : 'https://this-or-that-rileygrotenhuis.vercel.app';
